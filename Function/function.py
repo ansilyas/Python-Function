53b33ccb286a0ed31783aea80b93a83f53c97131
ANSILYAS <114913715+ansilyas@users.noreply.github.com>
2023-04-12 23:28:16 +0500

    Add files via upload

---
+++ Function/function.py
@@ -0,0 +1,30 @@
+# %%
+def rev(text):
+    return text[::-1]
+
+
+value = rev("ans,ali,text")
+print(value)
+# %%
+# Multipul or Single Value:
+
+
+def add():  # (Parameter)
+    a = 10
+    b = 20
+    c = a+b
+    d = a-b
+    return (c, d)
+
+
+sum, sub = add()  # This is calling of function (argument) :
+print(sum)
+print(sub)
+# %%
+
+def area(red):
+    total = 2*3.1415*red
+    return(total)
+print()
+
+# %%
